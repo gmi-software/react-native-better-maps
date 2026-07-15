5980c61f98069fad5cb4d577b799cc51ff37ab7d
Jakub Kasprzyk <kasprzykjakub@proton.me>
2026-07-15 11:30:52 +0200

    refactor: streamline user interaction detection in MKMapView

--- package/ios/MKMapView+userInteraction.swift
+++ package/ios/MKMapView+userInteraction.swift
@@ -2,12 +2,16 @@ import MapKit
 
 extension MKMapView {
   var isUserInteracting: Bool {
-    guard let recognizers = subviews.first?.gestureRecognizers else {
-      return false
+    return hasActiveGestureRecognizer(in: self)
+  }
+
+  private func hasActiveGestureRecognizer(in view: UIView) -> Bool {
+    if let recognizers = view.gestureRecognizers,
+      recognizers.contains(where: { $0.state == .began || $0.state == .changed })
+    {
+      return true
     }
 
-    return recognizers.contains { recognizer in
-      recognizer.state == .began || recognizer.state == .changed
-    }
+    return view.subviews.contains { hasActiveGestureRecognizer(in: $0) }
   }
 }
